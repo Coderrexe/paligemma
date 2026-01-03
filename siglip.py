0a68e59b465103e58581a550be6551880d041645
Coderrexe <shizimo@gmail.com>
2026-01-03 01:07:48 +0100

    finish all of siglip model architecture

--- siglip.py
+++ siglip.py
@@ -183,6 +183,28 @@ class SiglipEncoderLayer(nn.Module):
     return hidden_states
 
 
+class SiglipEncoder(nn.Module):
+  def __init__(self, config: SiglipVisionConfig):
+    super().__init__()
+    self.config = config
+    self.layers = nn.ModuleList(
+      [SiglipEncoderLayer(config) for _ in range(config.num_hidden_layers)]
+    )
+  
+  def forward(
+    self,
+    input_embeds: torch.Tensor
+  ) -> torch.Tensor:
+    # input_embeds: [batch_size, num_patches, embed_dim]
+    hidden_states = input_embeds
+
+    for encoder_layer in self.layers:
+      # [batch_size, num_patches, embed_dim] -> [batch_size, num_patches, embed_dim]
+      hidden_states = encoder_layer(hidden_states)
+    
+    return hidden_states
+
+
 class SiglipVisionTransformer(nn.Module):
   def __init__(self, config: SiglipVisionConfig):
     self.config = config
